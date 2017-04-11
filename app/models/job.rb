4d49c6b98a5433df77eaeff406c5556ad808db4c
李婉 <iwan16406783@gmail.com>
2017-04-12 06:57:33 +0800

    add Nameerror in ResumesController#new

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,11 @@
 class Job < ApplicationRecord
+
+  has_many :resumes
+  
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
+
+
   def publish!
   self.is_hidden = false
   self.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ApplicationRecord
 
 
   has_many :jobs
+  has_many :resumes
 end
